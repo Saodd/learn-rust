361ebd52bda2aa25479ba85372412c234d4890df
Lewin Lan <lewin.lan.cn@gmail.com>
2023-03-25 21:55:38 +0800

    5.3 Methods with More Parameters

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,31 @@
-use std::cmp::Ordering;
-use std::io::{stdin, stdout, Write};
+#![allow(dead_code)]
 
-use rand::Rng;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+    fn can_hold(&self, another: &Self) -> bool {
+        if self.width < another.width { return false; }
+        if self.height < another.height { return false; }
+        return true;
+    }
+}
 
 fn main() {
-    println!("—— Welcome to Guess Number Game ——");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 40,
+        ..rect1
+    };
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        print!("Pls guess a number: ");
-        stdout().flush().ok();
-
-        let mut input_string = String::new();
-        match stdin().read_line(&mut input_string) {
-            Ok(_) => {}
-            Err(err) => {
-                println!("[Error] while reading input: {}", err);
-                continue;
-            }
-        }
-
-        let input_number: u32 = match input_string.trim().parse::<u32>() {
-            Ok(num) => num,
-            Err(err) => {
-                println!("[Error] while parsing input: {}", err);
-                continue;
-            }
-        };
-
-        match input_number.cmp(&secret_number) {
-            Ordering::Less => {
-                println!("You guessed {input_number}, it's *Less* than the secret_number!")
-            }
-            Ordering::Equal => {
-                println!("Congratulations! The secret number is {secret_number}");
-                break;
-            }
-            Ordering::Greater => {
-                println!("You guessed {input_number}, it's *Greater* than the secret_number!")
-            }
-        }
-    }
+    println!("rect1 can hold rect2 ? {} ", rect1.can_hold(&rect2));
 }
