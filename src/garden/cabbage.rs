8278fc991d2dd233a88a9028e2dd70e5ad9a8766
Lewin Lan <lewin.lan.cn@gmail.com>
2023-03-26 13:29:40 +0800

    7.2 Defining Modules

---
+++ src/garden/cabbage.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+pub struct Cabbage {
+    days: u32,
+}
+
+impl Cabbage {
+   pub fn new() -> Self {
+        return Cabbage {
+            days: 0,
+        }
+    }
+
+    pub fn speak(&self) -> String {
+        format!("I'm {} days old!", self.days)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
-#![allow(dead_code)]
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        return self.width * self.height;
-    }
-    fn can_hold(&self, another: &Self) -> bool {
-        if self.width < another.width { return false; }
-        if self.height < another.height { return false; }
-        return true;
-    }
-}
+mod garden;
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 40,
-        ..rect1
-    };
-
-    println!("rect1 can hold rect2 ? {} ", rect1.can_hold(&rect2));
+    let cab = garden::cabbage::Cabbage::new();
+    println!("I'm growing {:?}, it says: {}", cab, cab.speak())
 }
