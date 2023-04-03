5265e542eb81ea9e499963b89f67945a66e1ffd5
Lewin Lan <lewin.lan.cn@gmail.com>
2023-04-03 08:23:58 +0800

    12-3 优化代码结构

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    let client = learn_rust::minigrep::Client::new(&args)?;
+    let (keyword, filepath) = parse_args()?;
+    let client = learn_rust::minigrep::Client::new(&keyword, &filepath)?;
     for line in client.search() {
         println!("{}", line)
     }
     Ok(())
+}
+
+fn parse_args() -> Result<(String, String), &'static str> {
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        return Err("you should input exact 3 args: xxx keyword filepath");
+    }
+    let keywords = args.remove(1);
+    let filepath = args.remove(1);
+
+    return Ok((keywords, filepath));
 }
--- src/minigrep.rs
+++ src/minigrep.rs
@@ -4,22 +4,11 @@ pub struct Client {
     file: String,
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum Error {
-    #[error("you should give exactly 3 args: executable keyword filepath")]
-    InvalidArgs,
-    #[error("std::io::Error: {0}")]
-    IOError(#[from] std::io::Error),
-}
-
 impl Client {
-    pub fn new(args: &Vec<String>) -> Result<Client, Error> {
-        if args.len() != 3 {
-            return Err(Error::InvalidArgs);
-        }
+    pub fn new(keyword: &str, filepath: &str) -> Result<Client, std::io::Error> {
         return Ok(Client {
-            word: String::from(&args[1]),
-            file: std::fs::read_to_string(&args[2])?,
+            word: String::from(keyword),
+            file: std::fs::read_to_string(filepath)?,
         });
     }
 
@@ -40,25 +29,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new_client_invalid_args() {
-        let args1: Vec<String> = vec![String::from("arg1")];
-        let args2: Vec<String> = vec![String::from("arg1"), String::from("arg2")];
-
-        for args in &vec![args1, args2] {
-            let res = Client::new(args);
-            assert!(matches!(res.unwrap_err(), Error::InvalidArgs))
-        }
-    }
-
-    #[test]
     fn new_client_invalid_filepath() {
-        let args: Vec<String> = vec![
-            String::from("arg1"),
-            String::from("arg2"),
-            String::from("some path that must not exists"),
-        ];
-        let err = Client::new(&args).unwrap_err();
-        assert!(matches!(err, Error::IOError(_)));
+        Client::new("keyword", "some path that must not exists").expect_err("should return error");
     }
 
     #[test]
