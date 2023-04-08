e6ad02eb8675f7abba2d36118a7e197d890e53e3
Lewin Lan <lewin.lan.cn@gmail.com>
2023-04-08 14:20:41 +0800

    leetcode: q0014

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,4 +1,5 @@
 mod q0001;
 mod q0007;
+mod q0014;
 
 pub struct Solution();
---
+++ src/leetcode/q0014.rs
@@ -0,0 +1,75 @@
+use std::str::Chars;
+
+use super::Solution;
+
+impl Solution {
+    /// 14. 最长公共前缀
+    ///
+    /// 编写一个函数来查找字符串数组中的最长公共前缀。
+    ///
+    /// 如果不存在公共前缀，返回空字符串 ""。
+    ///
+    /// 提示：
+    ///
+    /// 1 <= `strs.length` <= 200  
+    /// 0 <= `strs[i].length` <= 200  
+    /// `strs[i]` 仅由小写英文字母组成  
+    ///
+    /// 来源：力扣（LeetCode）  
+    /// 链接：<https://leetcode.cn/problems/longest-common-prefix/>  
+    /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。  
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut res = String::with_capacity(strs[0].len());
+
+        let mut its = strs.iter().map(|s| s.chars());
+        let it0: Chars = its.next().expect("1 <= strs.length");
+        let mut its: Vec<Chars> = its.collect();
+
+        for char0 in it0 {
+            for it in its.iter_mut() {
+                match it.next() {
+                    None => return res,
+                    Some(char1) => {
+                        if char0 != char1 {
+                            return res;
+                        }
+                    }
+                }
+            }
+            res.push(char0);
+        }
+
+        return res;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn prepare(strs: Vec<&str>) -> Vec<String> {
+        let mut res: Vec<String> = Vec::with_capacity(strs.len());
+        for s in strs {
+            res.push(s.to_string())
+        }
+        return res;
+    }
+
+    #[test]
+    fn examples() {
+        assert_eq!(
+            Solution::longest_common_prefix(prepare(vec!["flower", "flow", "flight"])),
+            "fl".to_string()
+        );
+        assert_eq!(
+            Solution::longest_common_prefix(prepare(vec!["dog", "racecar", "car"])),
+            "".to_string()
+        );
+    }
+
+    // #[test]
+    // fn check_myself() {}
+    //
+    // #[test]
+    // fn commit_failed() {}
+}
