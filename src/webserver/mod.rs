4d088758ed0647a6f1897a4c3d65efc09313cd34
Lewin Lan <lewin.lan.cn@gmail.com>
2023-04-30 17:20:46 +0800

    20.1.2 Reading the Request

--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
 
 pub struct Server {}
 
@@ -9,7 +10,18 @@ impl Server {
         for incoming in listener.incoming() {
             let stream = incoming.unwrap();
 
-            dbg!(stream);
+            Server::handle_stream(stream);
         }
+    }
+
+    fn handle_stream(stream: TcpStream) {
+        let buf = BufReader::new(stream);
+
+        let req: Vec<String> = buf
+            .lines()
+            .map(|line| line.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+        println!("Request: {:#?}", req);
     }
 }
