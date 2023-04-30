44fb068af51c34eab710971c0aa0a3858b7782fe
Lewin Lan <lewin.lan.cn@gmail.com>
2023-04-30 19:11:57 +0800

    20.1.4 自由发挥-长连接

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use learn_rust::webserver::Server;
 
 fn main() {
-    let server = Server::run("0.0.0.0:8080");
+    let mut server = Server::new();
+    server.get(
+        "/",
+        Box::new(|c| {
+            dbg!(&c);
+            let resp_body =
+                "<HTML><head><script>fetch('/api').then(resp=>resp.data())</script></head><body>hello, world!</body></HTML>";
+            let resp_body_len = resp_body.len();
+            let response =
+                format!("HTTP/1.1 200 OK\r\nContent-Length: {resp_body_len}\r\n\r\n{resp_body}");
+            c.response(response.as_bytes())
+        }),
+    );
+
+    server.run("0.0.0.0:8080");
 }
--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -1,33 +1,82 @@
+use std::collections::{HashMap, VecDeque};
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
-pub struct Server {}
+type Handler = Box<dyn Fn(&mut Context) -> ()>;
+
+#[derive(Debug)]
+pub struct Context<'a> {
+    stream: &'a mut TcpStream,
+}
+
+impl Context<'_> {
+    pub fn response(&mut self, data: &[u8]) {
+        self.stream.write_all(data).unwrap();
+    }
+}
+
+pub struct Server {
+    get_handlers: HashMap<String, Handler>,
+}
 
 impl Server {
-    pub fn run(addr: &str) {
+    pub fn new() -> Self {
+        return Self {
+            get_handlers: HashMap::new(),
+        };
+    }
+
+    pub fn get(&mut self, path: &str, handler: Handler) {
+        self.get_handlers.insert(String::from(path), handler);
+    }
+
+    pub fn run(&self, addr: &str) {
         let listener = TcpListener::bind(addr).unwrap();
 
         for incoming in listener.incoming() {
             let stream = incoming.unwrap();
 
-            Server::handle_stream(stream);
+            self.handle_stream(stream);
         }
     }
 
-    fn handle_stream(mut stream: TcpStream) {
-        let buf = BufReader::new(&stream);
+    fn handle_stream(&self, mut stream: TcpStream) {
+        println!("新的stream耶！");
+        loop {
+            let buf = BufReader::new(&stream);
+            let req_lines: Vec<String> = (buf)
+                .lines()
+                .map(|line| line.unwrap())
+                .take_while(|line| !line.is_empty())
+                .collect();
+            println!("Request: {:#?}", req_lines);
 
-        let req: Vec<String> = buf
-            .lines()
-            .map(|line| line.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
-        println!("Request: {:#?}", req);
+            let (method, path, http_version) = match req_lines.get(0) {
+                Some(line) => {
+                    let mut words: VecDeque<String> = line.split(' ').map(String::from).collect();
+                    (
+                        words.remove(0).unwrap(),
+                        words.remove(0).unwrap(),
+                        words.remove(0).unwrap(),
+                    )
+                }
+                None => return,
+            };
 
-        let resp_body = "hello, world!";
+            match self.get_handlers.get(&path) {
+                None => self.response_404(&mut stream),
+                Some(handler) => handler(&mut Context {
+                    stream: &mut stream,
+                }),
+            }
+        }
+    }
+
+    fn response_404(&self, stream: &mut TcpStream) {
+        let resp_body = "404 Not found~~";
         let resp_body_len = resp_body.len();
         let response =
-            format!("HTTP/1.1 200 OK\r\nContent-Length: {resp_body_len}\r\n\r\n{resp_body}");
+            format!("HTTP/1.1 404 Not found\r\nContent-Length: {resp_body_len}\r\n\r\n{resp_body}");
         stream.write_all(response.as_bytes()).unwrap();
     }
 }
